c0cb214def978a791b0167d6bd046b911eb29a77
Indelinac <150227292+Indelinac@users.noreply.github.com>
2024-06-09 16:46:05 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Build Status](https://codefirst.iut.uca.fr/api/badges/ines.delinac/Portfolio/status.svg)](https://codefirst.iut.uca.fr/ines.delinac/Portfolio)  
+
+# Portfolio
+
+Bienvenu sur mon portfolio, ceci est une première version, des améliorations sont à venir....
---
+++ main.js
@@ -0,0 +1,39 @@
+document.body.onload=function(){
+    nbr=2;
+    p=0;
+    container=document.getElementById("container");
+    g=document.getElementById("g");
+    d=document.getElementById("d");
+    container.style.width=(1000*nbr)+"px";
+    for(i=1;i<=nbr;i++){
+       div=document.createElement("div");
+       div.className="slide";
+       div.style.backgroundImage="url('media/im"+i+".png')";
+       container.appendChild(div);
+    };
+    afficherMasquer();
+ };
+ g.onclick=function(){
+    if(p<0)
+       p++;
+    container.style.transform="translate("+p*1000+"px)";
+    container.style.transition="all 0.5s ease";
+    afficherMasquer();
+ };
+ d.onclick=function(){
+    if(p>-nbr+1)
+       p--;
+    container.style.transform="translate("+p*1000+"px)";
+    container.style.transition="all 0.5s ease";
+    afficherMasquer();
+ };
+ function afficherMasquer() {
+    if(p==-nbr+1)
+       d.style.visibility="hidden";
+    else
+       d.style.visibility="visible";
+    if(p==0)
+       g.style.visibility="hidden";
+    else
+       g.style.visibility="visible";
+ }
